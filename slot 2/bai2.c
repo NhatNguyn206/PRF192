a52b47fb30cb876dc366541aba5b6707185d3706
NhatNguyn206 <nguyennhatnguyen2210@gmail.com>
2026-01-09 10:44:39 +0700

    slot2/bai2.c

---
+++ slot 2/bai2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+//khai bao 1 bien so nguyen
+// khoi tao gia tri bat ki
+//In ra man hinh 
+	int a = 22 ;
+	printf ("gia tri so nguyen cua ban la : %d",a);
+	return 0;
+		
+}
