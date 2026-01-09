ca8ed7ef71294e5e90583eba8e8fcd15b57ef2ab
NhatNguyn206 <nguyennhatnguyen2210@gmail.com>
2026-01-09 11:31:25 +0700

    slot2/bai4

---
+++ slot 2/bai4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+	float a = 5 ;
+	float b = 10.216;
+	printf ("a=%.0f\n",a);
+	printf ("b=%.3f\n",b);
+	float kq = 15.22;
+	printf ("5+10.216 = %.2f",kq);
+	return 0;
+	
+}
