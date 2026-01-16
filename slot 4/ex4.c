7910e69750dbdb4d5e086c2026b2b124cd4d5372
NhatNguyn206 <nguyennhatnguyen2210@gmail.com>
2026-01-16 11:29:00 +0700

    slot4

---
+++ slot 4/ex4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// TC1 khai báo a b nguyên , nhập a,b : 7,2   nhập phép toán : /     kết quả 7/2 = 3.50    
+//TC2 nhập a, b :7 ,0   nhập phép toán /       không thẻ chia không 
+int main (){
+	int a,b;
+	char c;
+	printf ("Nhap a , b :");
+	scanf ("%d,%d",&a,&b);
+	printf("Nhap phep toan: ");
+	scanf(" %c", &c);
+	if (b!=0){
+		printf("Ket qua : %d / %d = %.2f\n", a, b, (float)a / b);
+	}else{
+		printf ("Khong the chia cho 0");
+	}
+	
+	return 0;
+}
