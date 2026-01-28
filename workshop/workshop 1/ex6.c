844f44d441b76239309bd79063ef0a89f10db03d
NhatNguyn206 <nguyennhatnguyen2210@gmail.com>
2026-01-28 08:13:29 +0700

    workshop1

--- workshop/workshop 1/ex6.c
+++ workshop/workshop 1/ex6.c
@@ -7,10 +7,7 @@ int main(){
     if (scanf("%f %f %f", &cc, &gk, &ck) != 3 || cc < 0 || gk < 0 || ck < 0)
         return printf("Mot trong cac diem khong hop le!\n"), 0;
 
-		if (cc < 4 || gk < 4 || ck < 4 || diemChu == 'F'){
-		printf("Khong du dieu kien tot nghiep!");
-		return 0;
-		}
+
 	dtb = (cc*0.1)+(gk*0.3)+(ck*0.6);
 	printf ("Diem tong ket:%.2f\n",dtb);
 	if (dtb>=8.5 && dtb <=10){
@@ -25,6 +22,10 @@ int main(){
 		diemChu ='F';
 	}
 	printf("Diem chu la: %c\n",diemChu);
+	if (cc < 4 || gk < 4 || ck < 4 || diemChu == 'F'){
+		printf("Khong du dieu kien tot nghiep!");
+		return 0;
+	}
 	printf ("|%-15s|%-15s|%-15s|%-15s|%-15s|\n","Diem chuyen can","Diem giua ki","Diem cuoi ki","Diem tong ket","Diem chu la");
 	printf ("|%-15.2f|%-15.2f|%-15.2f|%-15.2f|%-15c|\n",cc,gk,ck,dtb,diemChu);
 	return 0;
